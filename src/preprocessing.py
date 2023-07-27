a4aa679044692291d365395f2156bc2c2bd54d36
benjwong <benjaminwong25@gmail.com>
2023-07-27 22:22:37 +0200

    test pre commit

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -32,5 +32,5 @@ def test_function(a:int, b:int) -> int:
 
 
     c = "Can you walk on the water if I, you and I Cause your blood s running cold outside the familiar, true to life Can you walk on the water if I, you and I Or keep your eyes on the road and live in the familiar, without you and I?"
-   
+    d = "Random change"
     return a + b + b 
