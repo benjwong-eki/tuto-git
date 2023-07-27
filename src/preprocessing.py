853728484abb651645683aba59a7b5f69059d586
benjwong <benjaminwong25@gmail.com>
2023-07-27 22:16:27 +0200

    adding src

---
+++ src/preprocessing.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pandas as pd 
+
+def add(x,y):
+    return x + y 
+
+def sub(x,y):
+    return x - y 
+
+def multiply(x,y):
+    if x == y:
+        return x*x
+    else:
+        return x*y
+
+def addition(x,y):
+    return x+y
+
+def test_function(a:int, b:int) -> int:
+    """Test 
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        int: _description_
+    """
+
+
+
+
+
+    c = "Can you walk on the water if I, you and I Cause your blood s running cold outside the familiar, true to life Can you walk on the water if I, you and I Or keep your eyes on the road and live in the familiar, without you and I?"
+   
+    return a + b + b 
